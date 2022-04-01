ddb01a7e16617b2da97ec8eb952ddf5c41c18fe9
kyrel <kyrel@LAPTOP-HTLVIMFU>
2022-04-01 14:57:53 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,364 @@
+﻿namespace W6__Latihan_Jumat
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_satu = new System.Windows.Forms.Button();
+            this.btn_dua = new System.Windows.Forms.Button();
+            this.btn_tiga = new System.Windows.Forms.Button();
+            this.btn_empat = new System.Windows.Forms.Button();
+            this.btn_lima = new System.Windows.Forms.Button();
+            this.btn_enam = new System.Windows.Forms.Button();
+            this.btn_tujuh = new System.Windows.Forms.Button();
+            this.btn_delapan = new System.Windows.Forms.Button();
+            this.btn_sembilan = new System.Windows.Forms.Button();
+            this.btn_backspace = new System.Windows.Forms.Button();
+            this.txtboxinput = new System.Windows.Forms.TextBox();
+            this.btn_clear = new System.Windows.Forms.Button();
+            this.btn_exit = new System.Windows.Forms.Button();
+            this.btn_tambah = new System.Windows.Forms.Button();
+            this.btn_kurang = new System.Windows.Forms.Button();
+            this.btn_bagi = new System.Windows.Forms.Button();
+            this.btn_kali = new System.Windows.Forms.Button();
+            this.btn_titik = new System.Windows.Forms.Button();
+            this.btn_nol = new System.Windows.Forms.Button();
+            this.lbox_history = new System.Windows.Forms.ListBox();
+            this.btn_total = new System.Windows.Forms.Button();
+            this.btn_maju = new System.Windows.Forms.Button();
+            this.btn_mundur = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(142, 96);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = ".";
+            // 
+            // btn_satu
+            // 
+            this.btn_satu.Location = new System.Drawing.Point(68, 310);
+            this.btn_satu.Name = "btn_satu";
+            this.btn_satu.Size = new System.Drawing.Size(100, 121);
+            this.btn_satu.TabIndex = 1;
+            this.btn_satu.Text = "1";
+            this.btn_satu.UseVisualStyleBackColor = true;
+            this.btn_satu.Click += new System.EventHandler(this.btn_satu_Click);
+            this.btn_satu.KeyDown += new System.Windows.Forms.KeyEventHandler(this.btn_satu_KeyDown);
+            // 
+            // btn_dua
+            // 
+            this.btn_dua.Location = new System.Drawing.Point(191, 310);
+            this.btn_dua.Name = "btn_dua";
+            this.btn_dua.Size = new System.Drawing.Size(100, 121);
+            this.btn_dua.TabIndex = 2;
+            this.btn_dua.Text = "2";
+            this.btn_dua.UseVisualStyleBackColor = true;
+            this.btn_dua.Click += new System.EventHandler(this.btn_dua_Click);
+            // 
+            // btn_tiga
+            // 
+            this.btn_tiga.Location = new System.Drawing.Point(328, 310);
+            this.btn_tiga.Name = "btn_tiga";
+            this.btn_tiga.Size = new System.Drawing.Size(100, 121);
+            this.btn_tiga.TabIndex = 3;
+            this.btn_tiga.Text = "3";
+            this.btn_tiga.UseVisualStyleBackColor = true;
+            this.btn_tiga.Click += new System.EventHandler(this.btn_tiga_Click);
+            // 
+            // btn_empat
+            // 
+            this.btn_empat.Location = new System.Drawing.Point(68, 455);
+            this.btn_empat.Name = "btn_empat";
+            this.btn_empat.Size = new System.Drawing.Size(100, 121);
+            this.btn_empat.TabIndex = 4;
+            this.btn_empat.Text = "4";
+            this.btn_empat.UseVisualStyleBackColor = true;
+            this.btn_empat.Click += new System.EventHandler(this.btn_empat_Click);
+            // 
+            // btn_lima
+            // 
+            this.btn_lima.Location = new System.Drawing.Point(191, 455);
+            this.btn_lima.Name = "btn_lima";
+            this.btn_lima.Size = new System.Drawing.Size(100, 121);
+            this.btn_lima.TabIndex = 5;
+            this.btn_lima.Text = "5";
+            this.btn_lima.UseVisualStyleBackColor = true;
+            this.btn_lima.Click += new System.EventHandler(this.btn_lima_Click);
+            // 
+            // btn_enam
+            // 
+            this.btn_enam.Location = new System.Drawing.Point(328, 455);
+            this.btn_enam.Name = "btn_enam";
+            this.btn_enam.Size = new System.Drawing.Size(100, 121);
+            this.btn_enam.TabIndex = 6;
+            this.btn_enam.Text = "6";
+            this.btn_enam.UseVisualStyleBackColor = true;
+            this.btn_enam.Click += new System.EventHandler(this.btn_enam_Click);
+            // 
+            // btn_tujuh
+            // 
+            this.btn_tujuh.Location = new System.Drawing.Point(68, 602);
+            this.btn_tujuh.Name = "btn_tujuh";
+            this.btn_tujuh.Size = new System.Drawing.Size(100, 121);
+            this.btn_tujuh.TabIndex = 7;
+            this.btn_tujuh.Text = "7";
+            this.btn_tujuh.UseVisualStyleBackColor = true;
+            this.btn_tujuh.Click += new System.EventHandler(this.btn_tujuh_Click);
+            // 
+            // btn_delapan
+            // 
+            this.btn_delapan.Location = new System.Drawing.Point(191, 602);
+            this.btn_delapan.Name = "btn_delapan";
+            this.btn_delapan.Size = new System.Drawing.Size(100, 121);
+            this.btn_delapan.TabIndex = 8;
+            this.btn_delapan.Text = "8";
+            this.btn_delapan.UseVisualStyleBackColor = true;
+            this.btn_delapan.Click += new System.EventHandler(this.btn_delapan_Click);
+            // 
+            // btn_sembilan
+            // 
+            this.btn_sembilan.Location = new System.Drawing.Point(328, 602);
+            this.btn_sembilan.Name = "btn_sembilan";
+            this.btn_sembilan.Size = new System.Drawing.Size(100, 121);
+            this.btn_sembilan.TabIndex = 9;
+            this.btn_sembilan.Text = "9";
+            this.btn_sembilan.UseVisualStyleBackColor = true;
+            this.btn_sembilan.Click += new System.EventHandler(this.btn_sembilan_Click);
+            // 
+            // btn_backspace
+            // 
+            this.btn_backspace.Location = new System.Drawing.Point(68, 224);
+            this.btn_backspace.Name = "btn_backspace";
+            this.btn_backspace.Size = new System.Drawing.Size(168, 65);
+            this.btn_backspace.TabIndex = 10;
+            this.btn_backspace.Text = "Backspace";
+            this.btn_backspace.UseVisualStyleBackColor = true;
+            this.btn_backspace.Click += new System.EventHandler(this.btn_backspace_Click);
+            // 
+            // txtboxinput
+            // 
+            this.txtboxinput.Enabled = false;
+            this.txtboxinput.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtboxinput.Location = new System.Drawing.Point(135, 109);
+            this.txtboxinput.Name = "txtboxinput";
+            this.txtboxinput.Size = new System.Drawing.Size(438, 35);
+            this.txtboxinput.TabIndex = 11;
+            // 
+            // btn_clear
+            // 
+            this.btn_clear.Location = new System.Drawing.Point(260, 224);
+            this.btn_clear.Name = "btn_clear";
+            this.btn_clear.Size = new System.Drawing.Size(168, 65);
+            this.btn_clear.TabIndex = 12;
+            this.btn_clear.Text = "Clear";
+            this.btn_clear.UseVisualStyleBackColor = true;
+            this.btn_clear.Click += new System.EventHandler(this.btn_clear_Click);
+            // 
+            // btn_exit
+            // 
+            this.btn_exit.Location = new System.Drawing.Point(447, 224);
+            this.btn_exit.Name = "btn_exit";
+            this.btn_exit.Size = new System.Drawing.Size(168, 65);
+            this.btn_exit.TabIndex = 13;
+            this.btn_exit.Text = "Exit Application";
+            this.btn_exit.UseVisualStyleBackColor = true;
+            this.btn_exit.Click += new System.EventHandler(this.btn_exit_Click);
+            // 
+            // btn_tambah
+            // 
+            this.btn_tambah.Location = new System.Drawing.Point(447, 310);
+            this.btn_tambah.Name = "btn_tambah";
+            this.btn_tambah.Size = new System.Drawing.Size(168, 57);
+            this.btn_tambah.TabIndex = 14;
+            this.btn_tambah.Text = "+";
+            this.btn_tambah.UseVisualStyleBackColor = true;
+            this.btn_tambah.Click += new System.EventHandler(this.btn_tambah_Click);
+            // 
+            // btn_kurang
+            // 
+            this.btn_kurang.Location = new System.Drawing.Point(447, 373);
+            this.btn_kurang.Name = "btn_kurang";
+            this.btn_kurang.Size = new System.Drawing.Size(168, 65);
+            this.btn_kurang.TabIndex = 15;
+            this.btn_kurang.Text = "-";
+            this.btn_kurang.UseVisualStyleBackColor = true;
+            this.btn_kurang.Click += new System.EventHandler(this.btn_kurang_Click);
+            // 
+            // btn_bagi
+            // 
+            this.btn_bagi.Location = new System.Drawing.Point(447, 455);
+            this.btn_bagi.Name = "btn_bagi";
+            this.btn_bagi.Size = new System.Drawing.Size(168, 65);
+            this.btn_bagi.TabIndex = 16;
+            this.btn_bagi.Text = "/";
+            this.btn_bagi.UseVisualStyleBackColor = true;
+            this.btn_bagi.Click += new System.EventHandler(this.btn_bagi_Click);
+            // 
+            // btn_kali
+            // 
+            this.btn_kali.Location = new System.Drawing.Point(447, 526);
+            this.btn_kali.Name = "btn_kali";
+            this.btn_kali.Size = new System.Drawing.Size(168, 65);
+            this.btn_kali.TabIndex = 17;
+            this.btn_kali.Text = "x";
+            this.btn_kali.UseVisualStyleBackColor = true;
+            this.btn_kali.Click += new System.EventHandler(this.btn_kali_Click);
+            // 
+            // btn_titik
+            // 
+            this.btn_titik.Location = new System.Drawing.Point(447, 673);
+            this.btn_titik.Name = "btn_titik";
+            this.btn_titik.Size = new System.Drawing.Size(168, 50);
+            this.btn_titik.TabIndex = 19;
+            this.btn_titik.Text = ".";
+            this.btn_titik.UseVisualStyleBackColor = true;
+            this.btn_titik.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // btn_nol
+            // 
+            this.btn_nol.Location = new System.Drawing.Point(191, 746);
+            this.btn_nol.Name = "btn_nol";
+            this.btn_nol.Size = new System.Drawing.Size(100, 100);
+            this.btn_nol.TabIndex = 20;
+            this.btn_nol.Text = "0";
+            this.btn_nol.UseVisualStyleBackColor = true;
+            this.btn_nol.Click += new System.EventHandler(this.btn_nol_Click);
+            // 
+            // lbox_history
+            // 
+            this.lbox_history.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbox_history.FormattingEnabled = true;
+            this.lbox_history.ItemHeight = 32;
+            this.lbox_history.Location = new System.Drawing.Point(135, 30);
+            this.lbox_history.Name = "lbox_history";
+            this.lbox_history.Size = new System.Drawing.Size(438, 68);
+            this.lbox_history.TabIndex = 22;
+            this.lbox_history.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
+            // 
+            // btn_total
+            // 
+            this.btn_total.Location = new System.Drawing.Point(447, 602);
+            this.btn_total.Name = "btn_total";
+            this.btn_total.Size = new System.Drawing.Size(168, 55);
+            this.btn_total.TabIndex = 18;
+            this.btn_total.Text = "=";
+            this.btn_total.UseVisualStyleBackColor = true;
+            this.btn_total.Click += new System.EventHandler(this.btn_total_Click);
+            // 
+            // btn_maju
+            // 
+            this.btn_maju.Location = new System.Drawing.Point(540, 760);
+            this.btn_maju.Name = "btn_maju";
+            this.btn_maju.Size = new System.Drawing.Size(75, 48);
+            this.btn_maju.TabIndex = 24;
+            this.btn_maju.Text = ">>";
+            this.btn_maju.UseVisualStyleBackColor = true;
+            this.btn_maju.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btn_mundur
+            // 
+            this.btn_mundur.Location = new System.Drawing.Point(447, 760);
+            this.btn_mundur.Name = "btn_mundur";
+            this.btn_mundur.Size = new System.Drawing.Size(75, 48);
+            this.btn_mundur.TabIndex = 25;
+            this.btn_mundur.Text = "<<";
+            this.btn_mundur.UseVisualStyleBackColor = true;
+            this.btn_mundur.Click += new System.EventHandler(this.btn_mundur_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(705, 858);
+            this.Controls.Add(this.btn_mundur);
+            this.Controls.Add(this.btn_maju);
+            this.Controls.Add(this.lbox_history);
+            this.Controls.Add(this.btn_nol);
+            this.Controls.Add(this.btn_titik);
+            this.Controls.Add(this.btn_total);
+            this.Controls.Add(this.btn_kali);
+            this.Controls.Add(this.btn_bagi);
+            this.Controls.Add(this.btn_kurang);
+            this.Controls.Add(this.btn_tambah);
+            this.Controls.Add(this.btn_exit);
+            this.Controls.Add(this.btn_clear);
+            this.Controls.Add(this.txtboxinput);
+            this.Controls.Add(this.btn_backspace);
+            this.Controls.Add(this.btn_sembilan);
+            this.Controls.Add(this.btn_delapan);
+            this.Controls.Add(this.btn_tujuh);
+            this.Controls.Add(this.btn_enam);
+            this.Controls.Add(this.btn_lima);
+            this.Controls.Add(this.btn_empat);
+            this.Controls.Add(this.btn_tiga);
+            this.Controls.Add(this.btn_dua);
+            this.Controls.Add(this.btn_satu);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Aplikasi Jumat ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_satu;
+        private System.Windows.Forms.Button btn_dua;
+        private System.Windows.Forms.Button btn_tiga;
+        private System.Windows.Forms.Button btn_empat;
+        private System.Windows.Forms.Button btn_lima;
+        private System.Windows.Forms.Button btn_enam;
+        private System.Windows.Forms.Button btn_tujuh;
+        private System.Windows.Forms.Button btn_delapan;
+        private System.Windows.Forms.Button btn_sembilan;
+        private System.Windows.Forms.Button btn_backspace;
+        private System.Windows.Forms.TextBox txtboxinput;
+        private System.Windows.Forms.Button btn_clear;
+        private System.Windows.Forms.Button btn_exit;
+        private System.Windows.Forms.Button btn_tambah;
+        private System.Windows.Forms.Button btn_kurang;
+        private System.Windows.Forms.Button btn_bagi;
+        private System.Windows.Forms.Button btn_kali;
+        private System.Windows.Forms.Button btn_titik;
+        private System.Windows.Forms.Button btn_nol;
+        private System.Windows.Forms.ListBox lbox_history;
+        private System.Windows.Forms.Button btn_total;
+        private System.Windows.Forms.Button btn_maju;
+        private System.Windows.Forms.Button btn_mundur;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,338 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace W6__Latihan_Jumat
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int counter = 0;
+
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_satu_Click(object sender, EventArgs e)
+        {
+            if (txtboxinput.Text == "0" && txtboxinput.Text != null)
+            {
+                txtboxinput.Text = "1";
+            }
+            else
+            {
+                txtboxinput.Text = txtboxinput.Text + "1";
+            }
+                
+        }
+
+        private void btn_dua_Click(object sender, EventArgs e)
+        {
+            if (txtboxinput.Text == "0" && txtboxinput.Text != null)
+            {
+                txtboxinput.Text = "2";
+            }
+            else
+            {
+                txtboxinput.Text = txtboxinput.Text + "2";
+            }
+        }
+
+        private void btn_tiga_Click(object sender, EventArgs e)
+        {
+            if (txtboxinput.Text == "0" && txtboxinput.Text != null)
+            {
+                txtboxinput.Text = "3";
+            }
+            else
+            {
+                txtboxinput.Text = txtboxinput.Text + "3";
+            }
+        }
+
+        private void btn_empat_Click(object sender, EventArgs e)
+        {
+            if (txtboxinput.Text == "0" && txtboxinput.Text != null)
+            {
+                txtboxinput.Text = "4";
+            }
+            else
+            {
+                txtboxinput.Text = txtboxinput.Text + "4";
+            }
+        }
+
+        private void btn_lima_Click(object sender, EventArgs e)
+        {
+            if (txtboxinput.Text == "0" && txtboxinput.Text != null)
+            {
+                txtboxinput.Text = "5";
+            }
+            else
+            {
+                txtboxinput.Text = txtboxinput.Text + "5";
+            }
+        }
+
+        private void btn_enam_Click(object sender, EventArgs e)
+        {
+            if (txtboxinput.Text == "0" && txtboxinput.Text != null)
+            {
+                txtboxinput.Text = "6";
+            }
+            else
+            {
+                txtboxinput.Text = txtboxinput.Text + "6";
+            }
+        }
+
+        private void btn_tujuh_Click(object sender, EventArgs e)
+        {
+            if (txtboxinput.Text == "0" && txtboxinput.Text != null)
+            {
+                txtboxinput.Text = "7";
+            }
+            else
+            {
+                txtboxinput.Text = txtboxinput.Text + "7";
+            }
+        }
+
+        private void btn_delapan_Click(object sender, EventArgs e)
+        {
+            if (txtboxinput.Text == "0" && txtboxinput.Text != null)
+            {
+                txtboxinput.Text = "8";
+            }
+            else
+            {
+                txtboxinput.Text = txtboxinput.Text + "8";
+            }
+        }
+
+        private void btn_sembilan_Click(object sender, EventArgs e)
+        {
+            if (txtboxinput.Text == "0" && txtboxinput.Text != null)
+            {
+                txtboxinput.Text = "9";
+            }
+            else
+            {
+                txtboxinput.Text = txtboxinput.Text + "9";
+            }
+        }
+
+        private void btn_nol_Click(object sender, EventArgs e)
+        {
+            
+            
+              txtboxinput.Text = txtboxinput.Text + "0";
+            
+        }
+        double firstnumber;
+        string operation;
+        private void btn_tambah_Click(object sender, EventArgs e)
+        {
+            firstnumber = Convert.ToDouble(txtboxinput.Text);
+            txtboxinput.Text = "0";
+            operation = "+";
+        }
+
+        private void btn_total_Click(object sender, EventArgs e)
+        {
+            double secondnumber;
+            double result;
+
+            secondnumber = Convert.ToDouble(txtboxinput.Text);
+           
+
+            if (operation == "+")
+            {
+                result = firstnumber + secondnumber;
+                txtboxinput.Text = Convert.ToString(result);
+                firstnumber = result;
+            }
+            if (operation == "-")
+            {
+                result = firstnumber - secondnumber;
+                txtboxinput.Text = Convert.ToString(result);
+                firstnumber = result;
+            }
+            if (operation == "*")
+            {
+                result = firstnumber * secondnumber;
+                txtboxinput.Text = Convert.ToString(result);
+                firstnumber = result;
+            }
+            if (operation == "/")
+            {
+                result = firstnumber / secondnumber;
+                txtboxinput.Text = Convert.ToString(result);
+                firstnumber = result;
+            }
+            lbox_history.Items.Add(txtboxinput.Text);
+            counter += 1;
+            txtboxinput.Text = "0";
+        }
+
+        private void btn_kurang_Click(object sender, EventArgs e)
+        {
+            firstnumber = Convert.ToDouble(txtboxinput.Text);
+            txtboxinput.Text = "0";
+            operation = "-";
+        }
+
+        private void btn_kali_Click(object sender, EventArgs e)
+        {
+            firstnumber = Convert.ToDouble(txtboxinput.Text);
+            txtboxinput.Text = "0";
+            operation = "*";
+        }
+
+        private void btn_bagi_Click(object sender, EventArgs e)
+        {
+            firstnumber = Convert.ToDouble(txtboxinput.Text);
+            txtboxinput.Text = "0";
+            operation = "/";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtboxinput.Text = "0";
+            this.KeyPreview = true;
+            
+                
+            
+        }
+
+        private void btn_exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btn_clear_Click(object sender, EventArgs e)
+        {
+            txtboxinput.Text="0";
+
+        }
+
+        private void btn_backspace_Click(object sender, EventArgs e)
+        {
+            
+            if ((String.Compare(txtboxinput.Text, " ") < 0))
+            {
+                txtboxinput.Text = txtboxinput.Text.Substring(0, txtboxinput.Text.Length - 1 + 1);
+            }
+            else
+            {
+                txtboxinput.Text = txtboxinput.Text.Substring(0, txtboxinput.Text.Length - 1);
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Back)
+            {
+                btn_backspace.PerformClick();
+
+            }
+            if (e.KeyCode == Keys.NumPad1 || e.KeyCode == Keys.D1)
+            {
+                btn_satu.PerformClick();
+
+            }
+            if (e.KeyCode == Keys.NumPad2 || e.KeyCode == Keys.D2)
+            {
+                btn_dua.PerformClick();
+
+            }
+            if (e.KeyCode == Keys.NumPad3 || e.KeyCode == Keys.D3 )
+            {
+                btn_tiga.PerformClick();
+
+            }
+            if (e.KeyCode == Keys.NumPad4 || e.KeyCode == Keys.D4)
+            {
+                btn_empat.PerformClick();
+
+            }
+            if (e.KeyCode == Keys.NumPad5 || e.KeyCode == Keys.D5)
+            {
+                btn_lima.PerformClick();
+
+            }
+            if (e.KeyCode == Keys.NumPad6 || e.KeyCode == Keys.D6)
+            {
+                btn_enam.PerformClick();
+
+            }
+            if (e.KeyCode == Keys.NumPad7 || e.KeyCode == Keys.D7)
+            {
+                btn_tujuh.PerformClick();
+
+            }
+            if (e.KeyCode == Keys.NumPad8 || e.KeyCode == Keys.D8 )
+            {
+                btn_delapan.PerformClick();
+
+            }
+            if (e.KeyCode == Keys.NumPad9 || e.KeyCode == Keys.D9)
+            {
+                btn_sembilan.PerformClick();
+
+            }
+            if (e.KeyCode == Keys.NumPad0 || e.KeyCode == Keys.D0)
+            {
+                btn_nol.PerformClick();
+
+            }
+        }
+
+        private void btn_satu_KeyDown(object sender, KeyEventArgs e)
+        {
+            
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+                
+            if (lbox_history.SelectedIndex == counter)
+            {
+                lbox_history.SelectedIndex -= 2;
+                MessageBox.Show("Data sudah max");
+            }
+            else 
+            {
+                lbox_history.SelectedIndex += 1;
+            }
+        }
+
+        private void btn_mundur_Click(object sender, EventArgs e)
+        {
+            if(lbox_history.SelectedIndex < 0)
+            {
+                lbox_history.SelectedIndex = 0;
+            }
+            if (lbox_history.SelectedIndex > 0)
+            {
+                lbox_history.SelectedIndex = -1;
+            }
+        }
+    }
+}
